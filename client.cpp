ff3c9fcb3c78c41b1ad5dced81435bfa59ce35ca
AnuchkinArtem <rubidium128@gmail.com>
2023-12-09 13:55:26 +0300

    Добавил функции для создания клавиатур в телеграмм боте

--- client.cpp
+++ client.cpp
@@ -20,3 +20,28 @@ using namespace TgBot;
 
 #define getcwd _getcwd
 #define chdir _chdir
+
+
+void createOneColumnKeyboard(const vector<string>& buttonStrings, ReplyKeyboardMarkup::Ptr& kb)
+{
+    for (size_t i = 0; i < buttonStrings.size(); ++i) {
+        vector<KeyboardButton::Ptr> row;
+        KeyboardButton::Ptr button(new KeyboardButton);
+        button->text = buttonStrings[i];
+        row.push_back(button);
+        kb->keyboard.push_back(row);
+    }
+}
+
+void createKeyboard(const vector<vector<string>>& buttonLayout, ReplyKeyboardMarkup::Ptr& kb)
+{
+    for (size_t i = 0; i < buttonLayout.size(); ++i) {
+        vector<KeyboardButton::Ptr> row;
+        for (size_t j = 0; j < buttonLayout[i].size(); ++j) {
+            KeyboardButton::Ptr button(new KeyboardButton);
+            button->text = buttonLayout[i][j];
+            row.push_back(button);
+        }
+        kb->keyboard.push_back(row);
+    }
+}
