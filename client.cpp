5dec2565b199f18a2295a75cf5a51f3dbbd434e6
AnuchkinArtem <rubidium128@gmail.com>
2023-12-09 14:02:48 +0300

    Добавил запуск ПО

--- client.cpp
+++ client.cpp
@@ -129,4 +129,25 @@ int main() {
         }
 
         });
+
+    signal(SIGINT, [](int s) {
+        printf("SIGINT got\n");
+        exit(0);
+        });
+
+    try {
+        printf("Bot username: %s\n", bot.getApi().getMe()->username.c_str());
+        bot.getApi().deleteWebhook();
+
+        TgLongPoll longPoll(bot);
+        while (true) {
+            longPoll.start();
+        }
+    }
+    catch (exception& e) {
+        printf("error: %s\n", e.what());
+    }
+
+    return 0;
+
 }
